80db0368958bebe7a39eb3635ab3ca8f7ae4947f
Ackerman, Adam P. <adam@ou.edu>
2026-08-27 12:47:22 -0500

    Completed problem 1.3

---
+++ 3013_Review/Part1.2/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+ Website Password Validator
+Ask the user to enter a proposed password.
+
+A valid password must:
+
+Be at least 8 characters long
+Contain at least one number
+Contain at least one uppercase letter
+Display whether the password is valid. 
+    If it isn't, tell the user which requirements are missing.
+
+ */
+
+Console.WriteLine("Enter a proposed password:");
+string password = Console.ReadLine();
+
+bool isPasswordLongEnough = MeetsLengthRequirement(password);
+bool hasUppercaseLetter = MeetsUppercaseRequirement(password);
+bool hasNumber = MeetsNumberRequirement(password);
+
+if (isPasswordLongEnough && hasUppercaseLetter && hasNumber)
+{
+    Console.WriteLine("Password is valid.");
+
+}
+else
+{
+    Console.WriteLine("Password is invalid. Please ensure your password meets the following requirements:");
+    if (!isPasswordLongEnough)
+    {
+        Console.WriteLine("- Be at least 8 characters long");
+    }
+    if (!hasUppercaseLetter)
+    {
+        Console.WriteLine("- Contain at least one uppercase letter");
+    }
+    if (!hasNumber)
+    {
+        Console.WriteLine("- Contain at least one number");
+    }
+}
+
+bool MeetsUppercaseRequirement(string? password)
+{
+    bool isValid = false;
+
+    foreach (var character in password)
+    {
+        if (char.IsUpper(character) == true)
+        {
+            isValid = true;
+            break;
+        }
+    }
+
+    return isValid;
+}
+bool MeetsNumberRequirement(string? password)
+{
+    bool isValid = false;
+
+    foreach (var character in password)
+    {
+        if (char.IsDigit(character) == true)
+        {
+            isValid = true;
+            break;
+        }
+    }
+
+    return isValid;
+}
+bool MeetsLengthRequirement(string? password)
+{
+    bool isValid = false;
+
+    if (password.Length >= 8)
+    {
+        isValid = true;
+    }
+
+    return isValid;
+}
---
+++ 3013_Review/Part1.3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ Create a list containing the inventory quantities for several products:
+
+12, 4, 0, 18, 3, 9, 0, 6
+Loop through the inventory and determine:
+
+How many products are out of stock
+How many have low inventory (1–5)
+How many have sufficient inventory (>5)
+Total number of items in inventory
+
+ */
+//                                               0  1  2  3   4  5  6  7
+List<int> inventoryQuantities = new List<int> { 12, 4, 0, 18, 3, 9, 0, 6 };
+int numberOfOutOfStockProducts = 0;
+int numberOfLowInventoryProducts = 0;
+int numberOfSufficientInventoryProducts = 0;
+int totalNumberOfItemsInInventory = 0;
+foreach (var item in inventoryQuantities)
+{
+    if (item == 0)
+    {
+        numberOfOutOfStockProducts++;
+        //numberOfOutOfStockProducts = numberOfOutOfStockProducts + 1;
+    }
+    else if (item >= 1 && item <= 5)
+    {
+        numberOfLowInventoryProducts++;
+    }
+    else if (item > 5)
+    {
+        numberOfSufficientInventoryProducts++;
+    }
+
+    totalNumberOfItemsInInventory += item;
+}
+
+Console.WriteLine($"Number of out-of-stock products: {numberOfOutOfStockProducts}");
+Console.WriteLine($"Number of low-inventory products: {numberOfLowInventoryProducts}");
+Console.WriteLine($"Number of sufficient-inventory products: {numberOfSufficientInventoryProducts}");
+Console.WriteLine($"Total number of items in inventory: {totalNumberOfItemsInInventory}");
+
+//inventoryQuantities.Add(12);
+//inventoryQuantities.Add(4); 
